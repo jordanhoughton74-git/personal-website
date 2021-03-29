6ded5ad7a76cd47800c0c8cb0ade2d0b5345070f
Jordan Houghton <jordanhoughton74@gmail.com>
2021-03-29 15:16:46 +0100

    set div height for large screen

--- components/footer.jsx
+++ components/footer.jsx
@@ -9,7 +9,9 @@ function Footer(){
     return (
         <>
         <div className={styles.footer}>
-           <p className={styles.link}>Email: <a className={styles.recolor} href="mailto:info@houghtonwebdesign.co.uk">info@houghtonwebdesign.co.uk</a> Website by <a className={styles.recolor} href="https://houghtonwebdesign.co.uk/"> HWD</a></p>
+           <p className={styles.link}>Email: <a className={styles.recolor} href="mailto:info@houghtonwebdesign.co.uk">
+               info@houghtonwebdesign.co.uk</a> 
+                <br className={styles.lineBreak}/><span className={styles.addPad}> Website by <a className={styles.recolor} href="https://houghtonwebdesign.co.uk/"> HWD</a></span></p>
            <Link href="/credit"><a className={styles.credit}>Credit</a></Link>
         </div>
       
