2842aebcd8d0f4c3b2571b3fcae7344ad85b23a6
Jordan Houghton <jordanhoughton74@gmail.com>
2021-08-27 13:41:48 +0100

    fix footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,9 +1,11 @@
 
 export default function Footer() {
     return(
+        <>
         <footer className="text-center text-xl pb-10">
             <p className="pb-3">Houghton Web Design © 2021</p>
             <p>Website Created By <a target="_blank" rel="noopener norefferer" href="https://houghtonwebdesign.co.uk/portfolio" className="hover:text-indigo-600">Houghton Web Design</a></p>
         </footer>
+        </>
     )
 }
