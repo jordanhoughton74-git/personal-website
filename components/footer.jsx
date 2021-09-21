0ae122d44077ab7862814f295d9c8ce994ddf196
Jordan Houghton <79968865+jordanhoughton74-git@users.noreply.github.com>
2021-09-21 13:37:45 +0100

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
         <>
         <footer className="text-center text-xl pb-10">
             <p className="pb-3">Houghton Web Design © 2021</p>
-            <p>Website Created By <a target="_blank" rel="noopener norefferer" href="https://houghtonwebdesign.co.uk/portfolio" className="hover:text-indigo-600">Houghton Web Design</a></p>
+            <p>Website Created By <a target="_blank" rel="noopener norefferer" href="https://houghtonwebdesign.co.uk" className="hover:text-indigo-600">Houghton Web Design</a></p>
         </footer>
         </>
     )
